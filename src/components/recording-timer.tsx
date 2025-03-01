31ed958a7c2ac53132f8e4d84510cc62f01a4814
Błażej Bartoszewski <blazejbart@gmail.com>
2025-03-02 00:31:52 +0100

    refactor(utils): move formatTime function to utils.ts

--- src/components/recording-timer.tsx
+++ src/components/recording-timer.tsx
@@ -1,17 +1,12 @@
+"use client";
+
+import React from "react";
+import { formatTime } from "@/lib/utils";
+
 const RecordingTimer: React.FC<{
   isRecording: boolean;
   recordingTime: number;
 }> = ({ isRecording, recordingTime }) => {
-  const formatTime = (time: number) => {
-    const hours = Math.floor(time / 3600);
-    const minutes = Math.floor((time % 3600) / 60);
-    const seconds = time % 60;
-
-    return [hours, minutes, seconds]
-      .map((unit) => unit.toString().padStart(2, "0"))
-      .join(":");
-  };
-
   return (
     <h1
       className={`text-2xl font-semibold ${
--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,16 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { clsx, type ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
+}
+
+export function formatTime(time: number): string {
+  const hours = Math.floor(time / 3600);
+  const minutes = Math.floor((time % 3600) / 60);
+  const seconds = time % 60;
+
+  return [hours, minutes, seconds]
+    .map((unit) => unit.toString().padStart(2, "0"))
+    .join(":");
 }
